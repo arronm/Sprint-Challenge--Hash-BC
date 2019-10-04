8f2fae072d414f04cd401aecb980788be10b77ec
Arron M <marshall.arron@gmail.com>
2019-10-04 09:47:55 -0700

    Please the linting gods

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -4,19 +4,16 @@ from hashtables import (HashTable,
                         hash_table_retrieve)
 
 
-# input: weights = [ 4, 6, 10, 15, 16 ], length = 5, limit = 21
-# output: [ 3, 1 ] since these are the indices of weights 15 and 6 whose sum equals 21
-
 def get_indices_of_item_weights(weights, length, limit):
     ht = HashTable(16)
 
     for i in range(length):
-        target = limit - weights[i] # 4 // 4
-        check = hash_table_retrieve(ht, target) # None // 0
+        target = limit - weights[i]  # 4 // 4
+        check = hash_table_retrieve(ht, target)  # None // 0
         if check is not None:
-            return [i, check] # [1, 0]
+            return [i, check]  # [1, 0]
 
-        hash_table_insert(ht, weights[i], i) # {4: 0}
+        hash_table_insert(ht, weights[i], i)  # {4: 0}
 
     return None
 
@@ -27,6 +24,7 @@ def print_answer(answer):
     else:
         print("None")
 
+
 if __name__ == "__main__":
-    answer = get_indices_of_item_weights([ 4, 4 ], 2, 8)
+    answer = get_indices_of_item_weights([4, 4], 2, 8)
     print_answer(answer)
--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -1,22 +1,8 @@
 #  Hint:  You may not need all of these.  Remove the unused functions.
 from hashtables import (HashTable,
                         hash_table_insert,
-                        hash_table_remove,
-                        hash_table_retrieve,
-                        hash_table_resize)
+                        hash_table_retrieve)
 
-# tickets = [
-#   Ticket{ source: "PIT", destination: "ORD" },
-#   Ticket{ source: "XNA", destination: "CID" },
-#   Ticket{ source: "SFO", destination: "BHM" },
-#   Ticket{ source: "FLG", destination: "XNA" },
-#   Ticket{ source: "NONE", destination: "LAX" },
-#   Ticket{ source: "LAX", destination: "SFO" },
-#   Ticket{ source: "CID", destination: "SLC" },
-#   Ticket{ source: "ORD", destination: "NONE" },
-#   Ticket{ source: "SLC", destination: "PIT" },
-#   Ticket{ source: "BHM", destination: "FLG" }
-# ]
 
 class Ticket:
     def __init__(self, source, destination):
@@ -32,31 +18,18 @@ def reconstruct_trip(tickets, length):
     for i in range(length):
         # look for starting point while constructing hash
         hash_table_insert(hashtable, tickets[i].source, tickets[i].destination)
-    
+
+    # set initial location
     location = "NONE"
     for i in range(length):
         dest = hash_table_retrieve(hashtable, location)
-        # if dest == "NONE":
-        #     continue
-        # else:
         route[i] = dest
         location = dest
 
     return route
 
+
 if __name__ == "__main__":
-    # tickets = [
-    #     Ticket("PIT", "ORD"),
-    #     Ticket("XNA", "CID"),
-    #     Ticket("SFO", "BHM"),
-    #     Ticket("FLG", "XNA"),
-    #     Ticket("NONE", "LAX"),
-    #     Ticket("LAX", "SFO"),
-    #     Ticket("CID", "SLC"),
-    #     Ticket("ORD", "NONE"),
-    #     Ticket("SLC", "PIT"),
-    #     Ticket("BHM", "FLG")
-    # ]
     ticket_1 = Ticket("NONE", "PDX")
     ticket_2 = Ticket("PDX", "DCA")
     ticket_3 = Ticket("DCA", "NONE")
